9c5adf9f2ac37870320aaaeb28b192a553464d00
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-07 13:13:13 -0700

    Make session route singluar, Add pry-rails to gem, and run migrations

--- reddit/config/routes.rb
+++ reddit/config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 	resources :users, only: %i(new create)
-	resources :session, only: %i(new create destroy)
+	resource :session, only: %i(new create destroy)
 end
