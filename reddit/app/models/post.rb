15d6028d7177e44daea3c83b1ed1ca2d807eb5b6
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-07 23:04:26 -0700

    Update post and sub models

--- reddit/app/models/post.rb
+++ reddit/app/models/post.rb
@@ -2,8 +2,10 @@ class Post < ApplicationRecord
 	validates :title, presence: true 
 
 	belongs_to :author, 
-		primary_key: :id, 
 		foreign_key: :user_id, 
 		class_name: :User, 
 		inverse_of: :posts
+
+	has_many :post_subs, inverse_of: :post
+	has_many :subs, through: :post_subs, source: :sub
 end
