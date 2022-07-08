9ded319e0bbe2833175fc87e901ba15e2b4b6d95
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-07 19:42:02 -0700

    Create posts table, model, and routes

--- reddit/config/routes.rb
+++ reddit/config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root "subs#index"
 	resources :users, only: %i(new create show)
 	resource :session, only: %i(new create destroy)
-	resources :subs, expect: %i(destroy)
+	resources :subs, except: %i(destroy)
+	resources :posts, except: %i(index)
 end
--- reddit/db/schema.rb
+++ reddit/db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_011324) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_08_022928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "url"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "subs", force: :cascade do |t|
     t.string "name", null: false
